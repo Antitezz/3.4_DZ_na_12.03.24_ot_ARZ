ce2ed0b18bb505a90e52c7d94e76b89a3877ad82
Виктор <156666292+Antitezz@users.noreply.github.com>
2024-03-11 17:20:07 +0300

    Update and rename Class7.cs to Generator of integer.cs

---
+++ DZ_na_12.03.24_ot_ARZ/Generator of integer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class IntGenerator : IElementGenerator<int>
+{
+    private static Random _random = new Random();
+    public int GenerateRandom()
+    {
+        return _random.Next(0, 100);
+    }
+}
+
+
