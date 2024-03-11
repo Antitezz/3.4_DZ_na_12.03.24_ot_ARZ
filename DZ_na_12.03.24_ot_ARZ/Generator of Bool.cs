53d0737a666daf4e250ef803f265a5ebe99fe7a8
Виктор <156666292+Antitezz@users.noreply.github.com>
2024-03-11 17:23:16 +0300

    Update Generator of Bool.cs

--- DZ_na_12.03.24_ot_ARZ/Generator of Bool.cs
+++ DZ_na_12.03.24_ot_ARZ/Generator of Bool.cs
@@ -1,1 +1,14 @@
-
+class BoolGenerator: IElementGenerator<bool>
+{
+    private static Random _random = new Random();
+    public bool GenerateRandom()
+    {
+        bool flag = false;
+        int determinant = _random.Next(0, 2);
+        if (determinant == 1)
+        {
+            flag = true;
+        }
+        return flag;
+    }
+}
