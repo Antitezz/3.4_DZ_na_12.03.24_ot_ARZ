a662840b8eb81233d4d5b33fbd27ac8416756448
Виктор <156666292+Antitezz@users.noreply.github.com>
2024-03-11 16:21:34 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public abstract class Mainclass : InterfaceDimension
+{
+    public void PublicInputArray()
+    {
+        InputArray();
+    }
+    protected abstract void InputArray();
+    public void PublicRandomArray()
+    {
+        RandomArray();
+    }
+    protected abstract void RandomArray();
+    public void PublicCreateArray(bool consoleValues = false)
+    {
+        CreateArray(consoleValues);
+    }
+    protected abstract void CreateArray(bool consoleValues = false);
+    public abstract void MiddleValue();
+    public abstract void Print();
+    public abstract void CreateAgain(bool consoleValues = false);
+}
+
